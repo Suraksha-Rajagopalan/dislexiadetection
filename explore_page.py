9c1366b6d35cf663c313a0a1d42cadb9f6b62917
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-03-22 13:57:22 +0530

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -1,4 +1,4 @@
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
