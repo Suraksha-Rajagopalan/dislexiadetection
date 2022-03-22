d0a8d662b87da1b5956ede4fc100dcb4b12024e7
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-03-22 13:43:43 +0530

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
 
 img_width, img_height = 224, 224
 
@@ -41,3 +41,19 @@ def modeltrain():
     model.add(Dropout(0.5))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
+    
+    model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+    train_datagen = ImageDataGenerator(rescale=1. / 255,shear_range=0.2,zoom_range=0.2,
+                                   horizontal_flip=True)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    train_generator = train_datagen.flow_from_directory(train_data_dir,target_size=(img_width, img_height),
+                                                    batch_size=batch_size,class_mode='binary')
+    validation_generator = test_datagen.flow_from_directory(validation_data_dir,target_size=(img_width, img_height),
+                                                        batch_size=batch_size,class_mode='binary')
+    model.fit(train_generator,steps_per_epoch=nb_train_samples // batch_size,epochs=epochs,
+                    validation_data=validation_generator,
+                    validation_steps=nb_validation_samples // batch_size)
+    model.save_weights('model_saved.h5')
+modeltrain()
