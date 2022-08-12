eb46c02bef29e4fc570e848f9a7977b4143e5834
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2022-08-12 20:02:46 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import imp
-from operator import mod
-import streamlit as st
-from predict_page import main
-from explore_page import modeltrain
-st.sidebar.selectbox("Explore or Predict",("Explore","Predict"))
-modeltrain()
-main()
